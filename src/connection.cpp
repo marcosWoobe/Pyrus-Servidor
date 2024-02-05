4aac0eee1699e294e915d8c8637a9bd8a1802ab1
Davi Ribeiro <46770855+odaviribeiro@users.noreply.github.com>
2024-02-05 16:16:58 -0300

    j

--- src/connection.cpp
+++ src/connection.cpp
@@ -500,7 +500,7 @@ void Connection::internalSend(OutputMessage_ptr msg)
 	try
 	{
 		++m_pendingWrite;
-		m_writeTimer.expires_from_now(boost::posix_time::seconds(static_cast<long>(Connection::writeTimeout));
+		m_writeTimer.expires_from_now(boost::posix_time::seconds(static_cast<long>(Connection::writeTimeout)));
 		m_writeTimer.async_wait(boost::bind(&Connection::handleWriteTimeout,
 			boost::weak_ptr<Connection>(shared_from_this()), boost::asio::placeholders::error));
 
